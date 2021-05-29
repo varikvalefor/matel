2427ed9a8c1e110b2c8b909fb2890afcdfd0d15b
Varik Valefor <38414004+varikvalefor@users.noreply.github.com>
2021-05-29 01:21:39 -0400

    Base.hs: Miscellaneous crap is added.

--- src/Metal/Base.hs
+++ src/Metal/Base.hs
@@ -10,3 +10,22 @@ type MessageText = BS.ByteString;
 type Screen = Widget ();
 type Winda = Widget ();
 type User = (Identifier, HumanReadableName);
+
+-- ROOM RECORDS --------------------------------------------------------
+
+data Room = Room {
+  roomId :: Identifier
+} deriving (Eq, Read, Show);
+
+-- MESSAGE RECORDS -----------------------------------------------------
+
+data TextMess = TextMess {
+  body :: MessageText,
+  sender :: User
+} deriving (Eq, Read, Show);
+{- TextMess contains the non-encrypted content of a message. -}
+
+data CryptoMess = CryptoMess {
+  encryptedContent :: BS.ByteString
+} deriving (Eq, Read, Show);
+{- CryptoMess is an encrypted message. -}
