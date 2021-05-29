26181870f1c9049c06ccf1853c82effa01e50e6c
Varik Valefor <38414004+varikvalefor@users.noreply.github.com>
2021-05-28 20:59:42 -0400

    Create Colour.hs

---
+++ src/Colour.hs
@@ -0,0 +1,21 @@
+module Colour where
+type RGBSubValue = Int;
+type RGBValue = (RGBSubValue, RGBSubValue, RGBSubValue);
+{- RGBValue is a 3-tuple of a red value, a green value, and a blue
+ - value.  Each such value's minimum and maximum are 0 and 255,
+ - respectively. -}
+{- Matel is designed to work best with light themes; as such, the
+ - default colour scheme may not work well with all terminals.  Luckily,
+ - VARIK, being a cool guy, writes the Colour module such that modifying
+ - Matel's colours should be pretty easy. -}
+
+-- MESSAGES ------------------------------------------------------------
+messHighlight :: RGBValue;
+messHighlight = (255, 0, 0);
+
+messStd :: RGBValue;
+messStd = (0, 0, 0);
+
+-- HOWIE MANDEL... NO, HANDLES -----------------------------------------
+usernameClient :: RGBValue;
+usernameClient = (0, 64, 0);
