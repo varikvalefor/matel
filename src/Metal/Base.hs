f4019c1637f29c641753ce83d1b6688bb4a845d3
Varik Valefor <varikvalefor@aol.com>
2021-05-29 07:44:01 -0400

    Base.hs: The Room record receives a roomName field.

--- src/Metal/Base.hs
+++ src/Metal/Base.hs
@@ -45,7 +45,10 @@ type User = (Identifier, HumanReadableName);
 data Room = Room {
   -- | For all Room k, roomId k is the "non-human-readable" name of k,
   -- e.g., "#johnnykissassSuckupfest:matrix.varikose.god".
-  roomId :: Identifier
+  roomId :: Identifier,
+  -- | For all Room k, roomName k is the "human-readable" name of k,
+  -- e.g., "Johnny Kissass's Suck-Up Fest".
+  roomName :: HumanReadableName
 } deriving (Eq, Read, Show);
 
 -- MESSAGE RECORDS -----------------------------------------------------
