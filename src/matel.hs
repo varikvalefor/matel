64f355ef7028524dfcca7b23a544920f5294e2dd
Varik Valefor <varikvalefor@aol.com>
2021-05-29 03:00:25 -0400

    src/matel.hs: Documentation is added.

--- src/matel.hs
+++ src/matel.hs
@@ -2,9 +2,15 @@ import Metal.Base;
 import Control.Concurrent;
 import GUI;
 
+-- | Tap tap tap
+-- Keyboard tap tap
+-- Documentation?
+-- Get yer own, twit.
 main :: IO ();
 main = newEmptyMVar >>= \ comVar ->
   forkIO (fetchData comVar) >> summonTUI comVar;
 
+-- | For all MVar Winda k, fetchData k collects data from Matrix, parses
+-- this data appropriately, and outputs this data to k.
 fetchData :: MVar Winda -> IO ();
 fetchData comVar = putMVar comVar temporaryMessage;
