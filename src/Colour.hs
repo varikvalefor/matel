c890384c2470a07caf04b91b26e79332d3d061b9
Varik Valefor <varikvalefor@aol.com>
2021-05-29 05:02:35 -0400

    Colour.hs: Documentation improves.

--- src/Colour.hs
+++ src/Colour.hs
@@ -10,26 +10,32 @@
  -
  - $Header$ contains some predefined colours which Matel uses when Matel
  - draws Matel's TUI.
+ - Matel is designed to work best with light themes; as such, the
+ - default colour scheme may not work well with all terminals.  Luckily,
+ - VARIK, being a cool guy, writes the Colour module such that modifying
+ - Matel's colour scheme should be pretty easy.
  - -}
 
 module Colour where
+-- | RGBSubValue is an integer value.  For all RGBSubValue k,
+-- 0 <= k <= 255.  Until a means of ensuring such bounds is implemented,
+-- attempting to blow up Matel is not strongly advised.  ;^)
 type RGBSubValue = Int;
+-- | RGBValue is a 3-tuple of a red value, a green value, and a blue
+-- value.
 type RGBValue = (RGBSubValue, RGBSubValue, RGBSubValue);
-{- RGBValue is a 3-tuple of a red value, a green value, and a blue
- - value.  Each such value's minimum and maximum are 0 and 255,
- - respectively. -}
-{- Matel is designed to work best with light themes; as such, the
- - default colour scheme may not work well with all terminals.  Luckily,
- - VARIK, being a cool guy, writes the Colour module such that modifying
- - Matel's colours should be pretty easy. -}
 
 -- MESSAGES ------------------------------------------------------------
+-- | messHighlight is the colour of the highlighted message, although
+-- the meaning of such highlighting is yet to be determined.
 messHighlight :: RGBValue;
 messHighlight = (255, 0, 0);
-
+-- | messStd is the default message text colour.
 messStd :: RGBValue;
 messStd = (0, 0, 0);
 
 -- HOWIE MANDEL... NO, HANDLES -----------------------------------------
+-- | usernameClient is the colour in which the username of Matel's user
+-- should be written.
 usernameClient :: RGBValue;
 usernameClient = (0, 64, 0);
