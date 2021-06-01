7bbd6c6cd59335bbe945b08b45b13f4bc8d49223
Varik Valefor <varikvalefor@aol.com>
2021-06-01 05:43:49 -0400

    Colour.hs: Colour now uses Brick's inbuilt colour stuff.

--- src/Colour.hs
+++ src/Colour.hs
@@ -14,32 +14,24 @@
  - default colour scheme may not work well with all terminals.  Luckily,
  - VARIK, being a cool guy, writes the Colour module such that modifying
  - Matel's colour scheme should be pretty easy.
- -
- - The current design of Colour is extremely unstable and shall most
- - likely be eventually tossed in favour of a relatively "clean"
- - solution, e.g., actually using Brick to Brick's full potential.
  - -}
 
 module Colour where
--- | RGBSubValue is an integer value.  For all RGBSubValue k,
--- 0 <= k <= 255.  Until a means of ensuring such bounds is implemented,
--- attempting to blow up Matel is not strongly advised.  ;^)
-type RGBSubValue = Int;
--- | RGBValue is a 3-tuple of a red value, a green value, and a blue
--- value.
-type RGBValue = (RGBSubValue, RGBSubValue, RGBSubValue);
+import Brick.Util (fg);
+import Graphics.Vty.Attributes (Attr);
+import Graphics.Vty.Attributes.Color;
 
 -- MESSAGES ------------------------------------------------------------
 -- | messHight is the colour of the highlighted message, although
 -- the meaning of such highlighting is yet to be determined.
-messHight :: RGBValue;
-messHight = (255, 0, 0);
+messHight :: Attr;
+messHight = fg red;
 -- | messStd is the default message text colour.
-messStd :: RGBValue;
-messStd = (0, 0, 0);
+messStd :: Attr;
+messStd = fg black;
 
 -- HOWIE MANDEL... NO, HANDLES -----------------------------------------
 -- | usernameClient is the colour in which the username of Matel's user
 -- should be written.
-usernameClient :: RGBValue;
-usernameClient = (0, 64, 0);
+usernameClient :: Attr;
+usernameClient = fg $ rgbColor 0 64 0;
