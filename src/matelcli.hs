7fa469aa16391ef76b67ed46c74c9c99c0baa4bf
Varik Valefor <varikvalefor@aol.com>
2021-06-11 17:00:46 -0400

    matelcli's "send" command begins.

--- src/matelcli.hs
+++ src/matelcli.hs
@@ -11,6 +11,7 @@ determineAction :: [String] -> IO ()
 determineAction x
   | x == [] = error "I need a command, jack-ass."
   | com == "list" = list $ tail x
+  | com == "send" = send $ tail x
   | otherwise = error $ "An unrecognised command is input.  " ++
     "RTFM, punk."
   where
@@ -31,3 +32,16 @@ list k
   | otherwise = error $ "The police will be listing your injuries " ++
     "if you don't stop inputting crap."
   where l = k !! 0;
+
+send :: [String] -> IO ();
+send k
+  | k == [] = error "I need some arguments, fat-ass."
+  | msgType == "text" = error $ "Sending text-based messages is " ++
+    "currently unimplemented."
+  | msgType == "file" = error $ "Sending files is currently " ++
+    "unimplemented."
+  | otherwise = error $ "I ought to send you to the garbage " ++
+    "disposal, punk.  Read the fucking manual."
+  where
+  msgType :: String
+  msgType = k !! 0;
