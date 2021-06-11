c01f7a022120091cd4565d06563d36df0e488a36
Varik Valefor <varikvalefor@aol.com>
2021-06-11 19:28:42 -0400

    Metal.Base: Identifier is now a synonym of String; identifiers ONLY CONTAIN ASCII CHARACTERS.

--- src/Metal/Base.hs
+++ src/Metal/Base.hs
@@ -19,7 +19,7 @@ import qualified Data.ByteString as BS;
 
 -- | For all Identifier k, k is a "non-human-readable" name, e.g.,
 -- "@johnnykissass:matrix.varikose.god", as opposed to "Johnny Kissass".
-type Identifier = Stringth;
+type Identifier = String;
 -- | For all HumanReadableName x, x is a "human-readable" name, e.g.,
 -- "Asshat", as opposed to "@asshat:matrix.varikose.god".
 type HumanReadableName = Stringth;
