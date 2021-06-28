ecd389a47e40bf734a71c8c2142de8f3f7771c87
Varik Valefor <varikvalefor@aol.com>
2021-06-27 20:14:54 -0400

    src/Metal/MatrixAPI/LowLevel.hs: Documentation improves.

--- src/Metal/MatrixAPI/LowLevel.hs
+++ src/Metal/MatrixAPI/LowLevel.hs
@@ -18,36 +18,37 @@ import Metal.Room;
 import Metal.Messages.Standard;
 import Data.ByteString (pack);
 
--- | That stillUnfinishedStayTuned exists implies that Matel is
+-- | That @stillUnfinishedStayTuned@ exists implies that Matel is
 -- currently useless as a Matrix client.
--- stillUnfinishedStayTuned is removed when proper Matrix API support is
--- added to Matel.
+-- @stillUnfinishedStayTuned@ is removed when proper Matrix API support
+-- is added to Matel.
 stillUnfinishedStayTuned :: ();
 stillUnfinishedStayTuned = ();
 
--- For all (ByteData z, PublicKey k), encryptWKey z k encrypts z with k,
--- outputting the resulting ciphertext.
--- encryptWKey is currently nonfunctional.
+-- For all @('ByteData' z, 'PublicKey' k)@, @encryptWKey z k@ encrypts
+-- @z@ with @k@, outputting the resulting ciphertext.
+-- @encryptWKey@ is currently nonfunctional.
 encryptWKey :: ByteData -> PublicKey -> CipherByteData;
 encryptWKey text key = pack [];
 
--- For all (CipherByteData z, PrivateKey k), decryptTextWKey z k
--- decrypts z with k, outputting the resulting ByteData-based data.
--- decryptWKey is currently nonfunctional.
+-- For all @('CipherByteData' z, 'PrivateKey' k)@, @decryptTextWKey z k@
+-- decrypts @z@ with @k@, outputting the resulting 'ByteData'-based
+-- data.
+-- @decryptWKey@ is currently nonfunctional.
 decryptWKey :: CipherByteData -> PrivateKey -> ByteData;
 decryptWKey crip key = pack [];
 
--- | For all valid Matrix usernames k, for all valid accompanying
--- passwords p, login k p fetches an authorisation token for Matrix
--- account k.
+-- | For all valid Matrix usernames @k@, for all valid accompanying
+-- passwords @p@, @login k p@ fetches an authorisation token for Matrix
+-- account @k@.
 login :: Identifier -> Stringth -> IO ErrorCode;
 login youshallnot pass = return "login is unimplemented.";
 
--- | sendSync accesses the Matrix "sync" function.
--- sendSync "" fetches the most recent Matrix messages.
--- For all other k, sendSync k sends a "sync" query to Matrix such that
--- the "since" parameter of this query equals k, fetching all messages
--- which are sent after k.
+-- | @sendSync@ accesses the Matrix "sync" function.
+-- @sendSync ""@ fetches the most recent Matrix messages.
+-- For all other @k@, @sendSync k@ sends a "sync" query to Matrix such
+-- that the "since" parameter of this query equals @k@, fetching all
+-- messages which are sent after @k@.
 sendSync :: String -- ^ The desired value of the query's "since" field
          -> String -- ^ The domain name of the user's homeserver
          -> IO [StdMess];
