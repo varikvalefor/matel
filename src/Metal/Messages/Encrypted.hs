caab0d7e1dad2bb76c02621fa64605013854b3aa
Varik Valefor <varikvalefor@aol.com>
2021-06-27 20:18:37 -0400

    src/Metal/Messages/Encrypted.hs: Markup improves.

--- src/Metal/Messages/Encrypted.hs
+++ src/Metal/Messages/Encrypted.hs
@@ -18,29 +18,29 @@ import Metal.Base;
 
 -- | For all CryptoMess k, k is an encrypted Matrix message.
 data CryptoMess = CryptoMess {
-  -- | For all CryptoMess k, ciphertext k equals the content of the
-  -- "ciphertext" field of k's source.
+  -- | For all @'CryptoMess' k@, @ciphertext k@ equals the content of
+  -- the "ciphertext" field of @k@'s source.
   -- [SNAKE ROAD SIGN] Remember that the decrypted ciphertext may
   -- actually contain an attachment, as opposed to a text message.
   ciphertext :: Stringth,
-  -- | For all CryptoMess k, algorithm k equals the content of the
-  -- "algorithm" field of k's source.
+  -- | For all @'CryptoMess' k@, @algorithm k@ equals the content of the
+  -- "algorithm" field of @k@'s source.
   algorithm :: Stringth,
-  -- | For all CryptoMess k, device_id k equals the content of the
-  -- "device_id" field of k's source.
+  -- | For all @'CryptoMess' k@, @device_id k@ equals the content of the
+  -- "device_id" field of @k@'s source.
   device_id :: Stringth,
-  -- | For all CryptoMess k, relates_to k equals the content of the
-  -- "event_id" field of the "m.relates_to" dingus of k's source.
+  -- | For all @'CryptoMess' k@, @relates_to k@ equals the content of
+  -- the "event_id" field of the "m.relates_to" dingus of @k@'s source.
   relates_to :: Stringth,
-  -- | For all CryptoMess k, sender_key k equals the content of the
-  -- "sender_key" field of k's source, i.e., the public key of the
+  -- | For all @'CryptoMess' k@, @sender_key k@ equals the content of
+  -- the "sender_key" field of @k@'s source, i.e., the public key of the
   -- sender of the message.
   sender_key :: Stringth,
-  -- | For all CryptoMess k, session_id k equals the content of the
-  -- "session_id" field of k's source, i.e., the ID of the session which
-  -- sends the message.
+  -- | For all @'CryptoMess' k@, @session_id k@ equals the content of
+  -- the "session_id" field of @k@'s source, i.e., the ID of the session
+  -- which sends the message.
   session_id :: Stringth,
-  -- | For all CryptoMess k, sender k equals the content of the
-  -- "sender" field of k's source.
+  -- | For all @'CryptoMess' k@, @sender k@ equals the content of the
+  -- "sender" field of @k@'s source.
   sender :: Stringth
 } deriving (Eq, Mess, Read, Show);
