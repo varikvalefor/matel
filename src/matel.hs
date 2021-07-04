1540ce92bbb4c93cf8ff9b78dd58d29b60839e73
Varik Valefor <varikvalefor@aol.com>
2021-07-03 23:42:01 -0400

    matel.hs: The markup of fetchData's documentation improves.

--- src/matel.hs
+++ src/matel.hs
@@ -10,8 +10,8 @@ main :: IO ();
 main = newEmptyMVar >>= \comVar ->
   forkIO (fetchData comVar) >> summonTUI comVar;
 
--- | For all MVar Winda k, fetchData k collects data from Matrix, parses
--- this data appropriately, and outputs this data to k.
--- fetchData is currently unimplemented.
+-- | For all 'MVar' 'Winda' @k@, @fetchData k@ collects data from
+-- Matrix, parses this data appropriately, and outputs this data to @k@.
+-- @fetchData@ is currently unimplemented.
 fetchData :: MVar Winda -> IO ();
 fetchData = flip putMVar temporaryMessage;
