bcb08702d55969a721d59623d2ba92b6812dfae8
Varik Valefor <varikvalefor@aol.com>
2021-07-11 15:36:14 -0400

    src/Metal/MatrixAPI/LowLevel/Types.hs: A header is added.

--- src/Metal/MatrixAPI/LowLevel/Types.hs
+++ src/Metal/MatrixAPI/LowLevel/Types.hs
@@ -1,3 +1,17 @@
+{- |
+ - Module      :  $Header$
+ - Description :  $Header$ contains some types which LowLevel uses.
+ - Copyright   :  (c) Varik Valefor
+ - License     :  BSD-3-Clause
+ -
+ - Maintainer  :  varikvalefor@aol.com
+ - Stability   :  unstable
+ - Portability :  portable
+ -
+ - $Header$ contains the datatypes which are of use only to
+ - Metal.MatrixAPI.LowLevel.
+ - -}
+
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE DeriveGeneric #-}
 
