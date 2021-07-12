a88188ccc9fb57c107c8ada9d80dcecffd5b38c4
Varik Valefor <varikvalefor@aol.com>
2021-07-12 17:13:33 -0400

    src/Metal/Space.hs: Documentation: Markup improves.

--- src/Metal/Space.hs
+++ src/Metal/Space.hs
@@ -14,8 +14,8 @@
 module Metal.Space where
 import Metal.Base;
 
--- | For all @'Space' k@, @k@ is a Matrix space.
+-- | For all 'Space' @k@, @k@ is a Matrix space.
 data Space = Space {
-  -- | For all @'Space' k@, @spaceId k@ equals the identifier of @k@.
+  -- | For all 'Space' @k@, @spaceId k@ equals the identifier of @k@.
   spaceId :: Identifier
 } deriving (Eq, Read, Show);
