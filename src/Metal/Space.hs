96d8a82e00cac80525f9bb1ae7ac4e6677a25f81
Varik Valefor <varikvalefor@aol.com>
2021-07-19 18:05:18 -0400

    src/Metal/Space.hs: Space receives the "spaceRooms" field.

--- src/Metal/Space.hs
+++ src/Metal/Space.hs
@@ -2,9 +2,13 @@
 
 module Metal.Space where
 import Metal.Base;
+import Metal.Room;
 
 -- | For all 'Space' @k@, @k@ is a Matrix space.
 data Space = Space {
   -- | @spaceId k@ equals the identifier of @k@.
-  spaceId :: Identifier
+  spaceId :: Identifier,
+  -- | @spaceRooms k@ equals a list of all rooms which are contained
+  -- within @k@.
+  spaceRooms :: [Room]
 } deriving (Eq, Read, Show);
