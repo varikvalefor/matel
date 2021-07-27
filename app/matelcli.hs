a05c86a2fc18f9ae46a9adfccbb00c837d35ab17
Varik Valefor <varikvalefor@aol.com>
2021-07-27 18:54:06 -0400

    app/matelcli.hs: A long line is broken.

--- app/matelcli.hs
+++ app/matelcli.hs
@@ -207,4 +207,8 @@ runKick :: [String] -> Auth -> IO ();
 runKick k a
   | length k < 3 = error $ "I'll kick YOUR ass if you don't start " ++
     "giving me some actual directions."
-  | otherwise = kick Def.user {username = k !! 0} Def.room {roomId = k !! 1} (k !! 2) a >>= maybe (return ()) error;
+  | otherwise = kick Def.user {username = k !! 0}
+                     Def.room {roomId = k !! 1}
+                     (k !! 2)
+                     a >>=
+                maybe (return ()) error;
