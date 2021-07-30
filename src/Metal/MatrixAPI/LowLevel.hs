2efda5608f7dbc4689daed640132fe7153d124f7
Varik Valefor <varikvalefor@aol.com>
2021-07-30 05:19:14 -0400

    src/Metal/MatrixAPI/LowLevel.hs: sync: For the sake of consistency, s/T -- \^/T\n-- \^/.

--- src/Metal/MatrixAPI/LowLevel.hs
+++ src/Metal/MatrixAPI/LowLevel.hs
@@ -88,8 +88,10 @@ loginPass user =
 -- which results from signing in to Matrix.  The 'Left' value of
 -- @sync k g@ exists only if an error is present... and equals a
 -- description of such an error.
-sync :: Maybe String -- ^ The desired value of the query's "since" field
-     -> Auth -- ^ The authorisation deets
+sync :: Maybe String
+     -- ^ The desired value of the query's "since" field
+     -> Auth
+     -- ^ The authorisation deets
      -> IO (Either Stringth Stringth);
 sync since user =
   generateRequest >>= httpBS >>= return . responseToLeftRight
