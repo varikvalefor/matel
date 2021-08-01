a4cb0a01bf8a7400579cf9e0fed64392d73dabb8
Varik Valefor <varikvalefor@aol.com>
2021-08-01 19:56:24 -0400

    app/GetAuth.hs: s/HOME/[HOME DIRECTORY]/

--- app/GetAuth.hs
+++ app/GetAuth.hs
@@ -18,7 +18,7 @@ import qualified Metal.Default as Def;
 -- of Matel's user.
 --
 -- This authorisation-related information is read from
--- @$HOME/.config/matel@, whose formatting is described in Matel's
+-- @[HOME DIRECTORY]/.config/matel@, whose formatting is described in Matel's
 -- "README" file.
 getAuthorisationDetails :: IO Auth;
 getAuthorisationDetails =
