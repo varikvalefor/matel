e81ff88307229571b2020b5cff24b41f6efa6054
Varik Valefor <varikvalefor@aol.com>
2021-08-02 18:18:12 -0400

    src/Metal/User.hs: A redundant type specification is deleted.

--- src/Metal/User.hs
+++ src/Metal/User.hs
@@ -6,8 +6,7 @@ import Data.List (elemIndex);
 
 -- | 'User' holds data regarding an arbitrary Matrix user.
 data User = User {
-  -- | For all 'User' @k@, @username k@ equals the Matrix username of
-  -- @k@.
+  -- | @username k@ equals the Matrix username of @k@.
   username :: Identifier,
   -- | @password k@ equals the password of @k@.  For reasons which
   -- should be obvious, @password@ is only used for the user account of
