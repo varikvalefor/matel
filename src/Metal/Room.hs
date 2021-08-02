4d0af177c3b07c6377ad2597eca98dff3005917f
Varik Valefor <varikvalefor@aol.com>
2021-08-02 18:16:48 -0400

    src/Metal/Room.hs: Some redundant type specifications are deleted.

--- src/Metal/Room.hs
+++ src/Metal/Room.hs
@@ -5,20 +5,19 @@ import Metal.User;
 
 -- | For all 'Room' @k@, @k@ is a Matrix chatroom.
 data Room = Room {
-  -- | For all 'Room' @k@, @roomId k@ is the "non-human-readable"
-  -- identifier of k, e.g., "!wnmjpIJcdaBNfOJrSw:matrix.org".
+  -- | @roomId k@ is the "non-human-readable" identifier of k, e.g.,
+  -- "!wnmjpIJcdaBNfOJrSw:matrix.org".
   roomId :: Identifier,
-  -- | For all 'Room' @k@, @roomHid k@ is the "human-readable"
-  -- identifier of @k@, e.g., "#johnnykissassSuckupfest:matrix.org".
+  -- | @roomHid k@ is the "human-readable" identifier of @k@, e.g.,
+  -- "#johnnykissassSuckupfest:matrix.org".
   roomHumanId :: Identifier,
-  -- | For all 'Room' @k@, @roomName k@ is the "human-readable" name of
-  -- @k@, e.g., "Johnny Kissass's Suck-Up Fest".
+  -- | @roomName k@ is the "human-readable" name of @k@, e.g., "Johnny
+  -- Kissass's Suck-Up Fest".
   roomName :: HumanReadableName,
-  -- | For all 'Room' @k@, @members k@ is the list of the members of
-  -- @k@.  Matel does not sort members according to any particular
-  -- thing.
+  -- | @members k@ is the list of the members of @k@.  Matel does not
+  -- sort members according to any particular thing.
   members :: [User],
-  -- | For all 'Room' @k@, @topic k@ equals the topic of @k@.
+  -- | @topic k@ equals the topic of @k@.
   topic :: Stringth,
   -- | @isEncrypted k@ iff encryption is enabled within @k@.
   isEncrypted :: Bool,
