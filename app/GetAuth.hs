43df7d4fa4bfb36ace6d24e62b297a5bffa16c40
Varik Valefor <varikvalefor@aol.com>
2021-08-06 04:51:45 -0400

    app/GetAuth.hs: Markup improves.

--- app/GetAuth.hs
+++ app/GetAuth.hs
@@ -19,7 +19,7 @@ import qualified Metal.Default as Def;
 -- of Matel's user.
 --
 -- This authorisation-related information is read from
--- @[HOME DIRECTORY]/.config/matel@, whose formatting is described in
+-- @[HOME DIRECTORY]\/.config\/matel@, whose formatting is described in
 -- Matel's "README" file.
 getAuthorisationDetails :: IO Auth;
 getAuthorisationDetails =
