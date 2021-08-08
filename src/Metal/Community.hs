3799b9ad179428262d9e8fba445c3414865dce58
Varik Valefor <varikvalefor@aol.com>
2021-08-08 15:57:36 -0400

    src/Metal/Community.hs: The deprecated nature of the community is mentioned.

--- src/Metal/Community.hs
+++ src/Metal/Community.hs
@@ -3,6 +3,9 @@ module Metal.Community where
 import Metal.Base;
 
 -- | For all 'Community' @k@, @k@ is a Matrix community.
+--
+-- Note that communities are essentially deprecated and are largely
+-- replaced by 'Space's.
 data Community = Community {
   -- | @commId k@ equals the community ID of @k@, e.g.,
   -- "+haskell:matrix.org".
