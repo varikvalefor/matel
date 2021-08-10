74baf45dd3a4f7e42e58e099694b792b84ba1b5c
Varik Valefor <varikvalefor@aol.com>
2021-08-10 17:36:07 -0400

    src/Metal/MatrixAPI/HighLevel.hs: A sentence shortens.

--- src/Metal/MatrixAPI/HighLevel.hs
+++ src/Metal/MatrixAPI/HighLevel.hs
@@ -101,7 +101,7 @@ memberComms :: Auth
             -> IO [Community];
 memberComms a = error "memberComms is unimplemented.";
 
--- | @isSentToRoom g k a@ only if @g@ is sent to Matrix room @k@ from
+-- | @isSentToRoom g k a@ sends @g@ to Matrix room @k@ from
 -- the account which is specified in @a@.
 --
 -- @isSentToRoom g k a@ equals an IO-monadic @""@ if no problem is
