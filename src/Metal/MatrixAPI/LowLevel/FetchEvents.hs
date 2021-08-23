138fc65c7df85076e2248eb018d8ec80103aef91
Varik Valefor <varikvalefor@aol.com>
2021-08-22 20:20:31 -0400

    src/Metal/MatrixAPI/LowLevel/FetchEvents.hs begins.  Additionally, some stuff in the CABAL file is sorted.

---
+++ src/Metal/MatrixAPI/LowLevel/FetchEvents.hs
@@ -0,0 +1,30 @@
+-- | This module contains @'fetchEvents'@ and some things which support
+-- @'fetchEvents'@.
+module Metal.MatrixAPI.LowLevel.FetchEvents (fetchEvents) where
+import Metal.Auth;
+import Metal.Base;
+import Metal.Room;
+import Metal.User;
+import Data.Maybe;
+import Metal.Space;
+import Metal.Community;
+import Network.HTTP.Simple;
+import qualified Data.Text as T;
+import qualified Data.Aeson as A;
+import qualified Metal.Default as Def;
+import qualified Data.ByteString as BS;
+import qualified Data.ByteString.Lazy as BSL;
+
+-- | For all 'Event' @A@, @A@ describes a Matrix room event.
+class Event a where
+  -- | @fetchEvents k r a@ fetches all events of type @a@ from the room
+  -- which is specified in @r@.  The authorisation information which is
+  -- specified in @a@ is used to authenticate the query.
+  fetchEvents :: a
+              -- ^ The type of event which should be fetched
+              -> Room
+              -- ^ The room from which events should be fetched
+              -> Auth
+              -- ^ The authorisation information which is used to
+              -- authenticate the query
+              -> [a];
