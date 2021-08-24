b481ca8a2681b27349951f33aef368179787c3f1
Varik Valefor <varikvalefor@aol.com>
2021-08-24 19:47:10 -0400

    src/Metal/MatrixAPI/HighLevel.hs: Line wrapping improves.

--- src/Metal/MatrixAPI/HighLevel.hs
+++ src/Metal/MatrixAPI/HighLevel.hs
@@ -85,8 +85,8 @@ memberRooms a = joinedRooms a >>= maybeShowRms
   maybeShowRms = listRoomsMentioned >=> \t ->
     if any EE.isLeft t
       then error $ T.unpack $ justLeft $ head $ filter EE.isLeft t
-      -- @filter EE.isLeft@ is used to ensure that the fetched 'Left' value
-      -- actually exists; some values may be 'Right'-valued.
+      -- @filter EE.isLeft@ is used to ensure that the fetched 'Left'
+      -- value actually exists; some values may be 'Right'-valued.
       -- An error is tossed because something has probably gone horribly
       -- wrong if any 'Left' values are present.
       -- VARIK is willing to modify @memberRooms@ such that
