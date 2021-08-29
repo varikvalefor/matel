7dc53112d32868cdac2558f904c9c17c067c0138
Varik Valefor <varikvalefor@aol.com>
2021-08-28 21:54:04 -0400

    Metal.Messages.Encrypted is now Metal.Encrypted... and uses EventCommonFields.

--- src/Metal/Encrypted.hs
+++ src/Metal/Encrypted.hs
@@ -1,15 +1,13 @@
 -- | Metal.Messages.Encrypted contains the source code of the
 -- 'CryptoMess' record type.
-module Metal.Messages.Encrypted where
+module Metal.Encrypted where
 import Metal.Base;
+import Metal.EventCommonFields;
 
 -- | For all 'CryptoMess' @k@, @k@ is an encrypted Matrix message.
-data CryptoMess = CryptoMess {
+data Encrypted = Encrypted {
   -- | @ciphertext k@ equals the content of the "ciphertext" field of
   -- @k@'s source.
-  --
-  -- [SNAKE ROAD SIGN] Remember that the decrypted ciphertext may
-  -- actually contain an attachment, as opposed to a text message.
   ciphertext :: Stringth,
   -- | @algorithm k@ equals the content of the "algorithm" field of
   -- @k@'s source.
@@ -17,16 +15,13 @@ data CryptoMess = CryptoMess {
   -- | @device_id k@ equals the content of the "device_id" field of
   -- @k@'s source.
   device_id :: Stringth,
-  -- | @relates_to k@ equals the content of the "event_id" field of the
-  -- "m.relates_to" dingus of @k@'s source.
-  relates_to :: Stringth,
   -- | @sender_key k@ equals the content of the "sender_key" field of
-  -- @k@'s source, i.e., the public key of the sender of the message.
+  -- @k@'s source, i.e., the public key of the sender of the event.
   sender_key :: Stringth,
   -- | @session_id k@ equals the content of the "session_id" field of
-  -- @k@'s source, i.e., the ID of the session which sends the message.
+  -- @k@'s source, i.e., the ID of the session which sends the event.
   session_id :: Stringth,
-  -- | @sender k@ equals the content of the "sender" field of @k@'s
-  -- source.
-  sender :: Stringth
+  -- | @boilerplate k@ contains the fields which all event types
+  -- contain.
+  boilerplate :: EventCommonFields
 } deriving (Eq, Read, Show);
