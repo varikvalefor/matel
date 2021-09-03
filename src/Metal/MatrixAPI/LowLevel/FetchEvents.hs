adcf7be6b5f4903d01a62512d1776ce1ae9ca917
Varik Valefor <varikvalefor@aol.com>
2021-09-03 16:31:06 -0400

    fetchEvents now actually returns messages, as opposed to spontaneously combusting.

--- src/Metal/MatrixAPI/LowLevel/FetchEvents.hs
+++ src/Metal/MatrixAPI/LowLevel/FetchEvents.hs
@@ -10,12 +10,17 @@ import Metal.User;
 import Data.Maybe;
 import Metal.Space;
 import Metal.Community;
+import Data.Aeson.Quick;
+import Control.Lens ((^?));
 import Network.HTTP.Simple;
+import Metal.EventCommonFields;
 import Metal.Messages.Standard;
 import Metal.OftenUsedFunctions;
 import qualified Data.Text as T;
 import qualified Data.Aeson as A;
+import Metal.MatrixAPI.LowLevel.Types;
 import qualified Metal.Default as Def;
+import qualified Data.Aeson.Lens as AL;
 import qualified Data.ByteString as BS;
 import qualified Data.ByteString.Lazy as BSL;
 import qualified Metal.MatrixAPI.LowLevel.HTTP as TP;
@@ -46,14 +51,52 @@ class Event a where
               -> IO [a];
 
 instance Event StdMess where
-  fetchEvents n d ms rm = process <.> TP.req TP.GET querr ""
+  fetchEvents n d ms rm = process <.> TP.req TP.GET querr bod
     where
     process :: Response BS.ByteString -> [StdMess]
     process k = case getResponseStatusCode k of
-      200 -> error "fetchEvents: process is unimplemented."
+      200 -> map toMessage $ (toValue k) .! "{chunk}"
       _   -> detroit k
+      where
+      toValue :: Response BS.ByteString -> Value
+      toValue = fromMaybe chunkMissing . decode . BSL.fromStrict .
+                getResponseBody
+      --
+      chunkMissing :: a
+      chunkMissing = error "Metal.MatrixAPI.LowLevel.FetchEvents.\
+                     \fetchEvents: The \"chunk\" field is absent!"
+    --
+    toMessage :: Value -> StdMess
+    toMessage k = case (k .! "{content:{msgtype}}" :: String) of
+      -- \^ Yes, the cheesy typecast _is_ necessary; GHC complains about
+      -- type ambiguity and refuses to compile this file if this
+      -- typecast is absent.
+      "m.text" -> Def.stdMess {
+                  body = k .! "{content:{body}}",
+                  fmtBody = k .? "{content:{formatted_body}}",
+                  -- \^ The "formatted_body" field _should_ be
+                  -- present... but _may_ not be present.
+                  boilerplate = Def.eventCommonFields {
+                    sender = Def.user {username = k .! "{sender}"},
+                    destRoom = Def.room {roomId = k .! "{room_id}"},
+                    eventId = k .! "{event_id}",
+                    origin_server_ts = k .! "{origin_server_ts}"
+                  }
+                }
+      _        -> error "fuck"
     --
     querr :: String
     querr = "_matrix/client/r0/rooms/" ++ roomId rm ++
-            "/messages?limit=" ++ show n ++ "&types=" ++
-            show (msgType ms) ++ "&dir=" ++ [d];
+            "/messages?limit=" ++ show n ++ "&types=m.room.message" ++
+            "&dir=" ++ [d]
+    --
+    bod :: BSL.ByteString
+    bod = fromString $
+          "{\n\t" ++
+            "\"filter\": {\n\t\t" ++
+              "\"types\": [\"m.room.message\"]\n\t" ++
+            "}\n" ++
+          "}"
+    --
+    fromString :: String -> BSL.ByteString
+    fromString = BSL.pack . map (toEnum . fromEnum);
