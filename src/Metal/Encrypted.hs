43016e2c8fa1ead7df64f27e94985b88359a8f0c
Varik Valefor <varikvalefor@aol.com>
2021-09-04 12:33:49 -0400

    src/Metal/Encrypted.hs: Variables replace words.

--- src/Metal/Encrypted.hs
+++ src/Metal/Encrypted.hs
@@ -6,18 +6,16 @@ import Metal.EventCommonFields;
 
 -- | For all 'CryptoMess' @k@, @k@ is an encrypted Matrix message.
 data Encrypted = Encrypted {
-  -- | @ciphertext k@ is the actual encrypted bit of this event.
+  -- | @ciphertext k@ is the actual encrypted bit of @k@.
   ciphertext :: Stringth,
   -- | @algorithm k@ is the algorithm which is used to encrypt @k@.
   algorithm :: Stringth,
   -- | @device_id k@ equals the content of the "device_id" field of
   -- @k@'s source.
   device_id :: Maybe Stringth,
-  -- | @sender_key k@ is the public key of the sender of this encrypted
-  -- event.
+  -- | @sender_key k@ is the public key of the sender of @k@.
   sender_key :: Stringth,
-  -- | @session_id k@ is the ID of the Matrix session which sends this
-  -- event.
+  -- | @session_id k@ is the ID of the Matrix session which sends @k@.
   session_id :: Maybe Stringth,
   -- | @boilerplate k@ contains the fields which all event types
   -- contain.
