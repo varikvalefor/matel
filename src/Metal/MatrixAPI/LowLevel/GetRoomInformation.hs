95229f7d22823972d2c1d0a6c242451a911c3bdc
Varik Valefor <varikvalefor@aol.com>
2021-09-06 12:25:03 -0400

    src/Metal/MatrixAPI/LowLevel/GetRoomInformation.hs receives a proper Haddock header.

--- src/Metal/MatrixAPI/LowLevel/GetRoomInformation.hs
+++ src/Metal/MatrixAPI/LowLevel/GetRoomInformation.hs
@@ -1,6 +1,14 @@
 {-# LANGUAGE OverloadedStrings #-}
 
--- | As a result of Metal.MatrixAPI.LowLevel's hugeness,
+-- | Module    : Metal.MatrixAPI.LowLevel.GetRoomInformation
+-- Description : Metal's stuff what fetches the information of rooms
+-- Copyright   : (c) Varik Valefor, 2021
+-- License     : BSD-3-Clause
+-- Maintainer  : varikvalefor@aol.com
+-- Stability   : unstable
+-- Portability : portable
+--
+-- As a result of Metal.MatrixAPI.LowLevel's hugeness,
 -- @'getRoomInformation'@ is moved from Metal.MatrixAPI.LowLevel to
 -- Metal.MatrixAPI.LowLevel.GetRoomInformation.
 module Metal.MatrixAPI.LowLevel.GetRoomInformation (
