74be2426a78f5a76decb2259b5aa848209eab16a
Varik Valefor <varikvalefor@aol.com>
2021-09-11 03:28:35 -0400

    src/Metal/MatrixAPI/LowLevel/Crypto/Miscellaneous.hs "begins".

---
+++ src/Metal/MatrixAPI/LowLevel/Crypto/Miscellaneous.hs
@@ -0,0 +1,10 @@
+-- | Module    : Metal.MatrixAPI.LowLevel.Crypto.Miscellaneous
+-- Description : Miscellaneous cryptographic functions
+-- Copyright   : (c) Varik Valefor, 2021
+-- License     : BSD-3-Clause
+-- Maintainer  : varikvalefor@aol.com
+-- Stability   : unstable
+-- Portability : portable
+--
+-- This module contains miscellaneous cryptographic functions.
+module Metal.MatrixAPI.LowLevel.Crypto.Miscellaneous where
