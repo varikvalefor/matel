3ec62b08532d342838e34193cd848c8493d964f6
Varik Valefor <varikvalefor@aol.com>
2021-09-12 15:40:38 -0400

    src/Metal/Messages/Standard.hs: s/Documentation/Useful documentation/

--- src/Metal/Messages/Standard.hs
+++ src/Metal/Messages/Standard.hs
@@ -14,7 +14,7 @@ import Metal.Messages.FileInfo;
 import Metal.Messages.EncryptedFile;
 
 -- | 'MessageType' is used to describe the types of the messages which
--- 'StdMess' records represent.  Documentation of this thing is visible
+-- 'StdMess' records represent.  Useful documentation of this thing is visible
 -- in the documentation of @'msgType'@; 'MessageType' is actually pretty
 -- useless alone.
 data MessageType = TextInnit
