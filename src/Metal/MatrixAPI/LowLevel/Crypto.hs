d774f42cf1a395cf3c9eeefa8e8b19c65bd587bc
Varik Valefor <varikvalefor@aol.com>
2021-09-14 19:14:22 -0400

    calcSecret is moved to LowLevel.Crypto.Miscellaneous.

--- src/Metal/MatrixAPI/LowLevel/Crypto.hs
+++ src/Metal/MatrixAPI/LowLevel/Crypto.hs
@@ -7,9 +7,7 @@
 -- Portability : portable
 --
 -- Metal.MatrixAPI.LowLevel.Crypto contains cryptographic crap.
-module Metal.MatrixAPI.LowLevel.Crypto (
-  calcSecret
-) where
+module Metal.MatrixAPI.LowLevel.Crypto where
 import Data.Maybe;
 import Metal.Base;
 import Crypto.Error;
@@ -17,23 +15,3 @@ import qualified Data.Text as T;
 import Metal.OftenUsedFunctions;
 import qualified Data.ByteString as BS;
 import qualified Crypto.PubKey.Curve25519 as X25519;
-
--- | @calcSecret a b@ is the shared secret key of @a@ and @b@.
-calcSecret :: PublicKey
-           -- ^ The public key of the recipient of the data with which
-           -- the resulting shared secret is encrypted
-           -> PrivateKey
-           -- ^ The private key of the sender of the data with which the
-           -- resulting shared secret is encrypted
-           -> X25519.DhSecret;
-calcSecret pu pr = X25519.dh pu' pr'
-  where
-  toBS :: T.Text -> BS.ByteString
-  toBS = fromString . T.unpack
-  --
-  pu' = yield $ X25519.publicKey $ toBS pu
-  --
-  pr' = yield $ X25519.secretKey (fromString $ T.unpack pr :: BS.ByteString)
-  --
-  yield :: CryptoFailable a -> a
-  yield = fromJust . maybeCryptoError;
--- src/Metal/MatrixAPI/LowLevel/Crypto/Miscellaneous.hs
+++ src/Metal/MatrixAPI/LowLevel/Crypto/Miscellaneous.hs
@@ -9,11 +9,15 @@
 -- This module contains miscellaneous cryptographic functions.
 module Metal.MatrixAPI.LowLevel.Crypto.Miscellaneous where
 import Data.Maybe;
+import Metal.Base;
 import Crypto.Error;
 import Crypto.Cipher.AES;
 import Crypto.Cipher.Types;
 import Crypto.Random.Types;
+import Metal.OftenUsedFunctions;
+import qualified Data.Text as T;
 import qualified Data.ByteString as BS;
+import qualified Crypto.PubKey.Curve25519 as X25519;
 
 -- | If @t@ is ciphertext, then @aes256CryptBS t sk iv@ is the result of
 -- decrypting @t@ with the secret key @sk@ and the initialisation vector
@@ -45,3 +49,23 @@ aes256CryptBS t sk iv = ctrCombine cipher (fromJust $ makeIV iv) t
 -- suitable for use as an AES initialisation vector or secret key.
 genIVorKeyBS :: IO BS.ByteString;
 genIVorKeyBS = getRandomBytes $ blockSize (undefined :: AES256);
+
+-- | @calcSecret a b@ is the shared secret key of @a@ and @b@.
+calcSecret :: PublicKey
+           -- ^ The public key of the recipient of the data with which
+           -- the resulting shared secret is encrypted
+           -> PrivateKey
+           -- ^ The private key of the sender of the data with which the
+           -- resulting shared secret is encrypted
+           -> X25519.DhSecret;
+calcSecret pu pr = X25519.dh pu' pr'
+  where
+  toBS :: T.Text -> BS.ByteString
+  toBS = fromString . T.unpack
+  --
+  pu' = yield $ X25519.publicKey $ toBS pu
+  --
+  pr' = yield $ X25519.secretKey (fromString $ T.unpack pr :: BS.ByteString)
+  --
+  yield :: CryptoFailable a -> a
+  yield = fromJust . maybeCryptoError;
