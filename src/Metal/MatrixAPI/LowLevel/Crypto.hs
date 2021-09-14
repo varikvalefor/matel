066f9fdba2e8f7a7845e698bd6505c914d763c25
Varik Valefor <varikvalefor@aol.com>
2021-09-14 19:13:15 -0400

    The Olm-based encryptWKey and decryptWKey are moved to LowLevel.Crypto.Olm.

--- src/Metal/MatrixAPI/LowLevel/Crypto.hs
+++ src/Metal/MatrixAPI/LowLevel/Crypto.hs
@@ -8,8 +8,6 @@
 --
 -- Metal.MatrixAPI.LowLevel.Crypto contains cryptographic crap.
 module Metal.MatrixAPI.LowLevel.Crypto (
-  encryptWKey,
-  decryptWKey,
   calcSecret
 ) where
 import Data.Maybe;
@@ -19,36 +17,6 @@ import qualified Data.Text as T;
 import Metal.OftenUsedFunctions;
 import qualified Data.ByteString as BS;
 import qualified Crypto.PubKey.Curve25519 as X25519;
-
--- | @encryptWKey z pu pr@ encrypts @z@ with the shared secret of public
--- key @pu@ and private key @pr@, outputting the resulting ciphertext.
---
--- @encryptWKey@ is IO-monadic because @encryptWKey@ uses functions
--- which return pseudorandom stuff, e.g., the AES-256 keys which encrypt
--- the actual data.
---
--- @encryptWKey@ is currently nonfunctional.
-encryptWKey :: ByteData
-            -- ^ The plaintext which should be encrypted
-            -> PublicKey
-            -- ^ The public key of the recipient of the encrypted thing
-            -> PrivateKey
-            -- ^ The private key of the sender of the encrypted thing
-            -> IO CipherByteData;
-encryptWKey text pu pr = error "encryptWKey is unimplemented.";
-
--- | @decryptWKey z pu pr@ decrypts @z@ with the shared secret of public
--- key @pu@ and private key @pr@, outputting the resulting cleartext.
---
--- @decryptWKey@ is currently nonfunctional.
-decryptWKey :: CipherByteData
-            -- ^ The ciphertext which should be decrypted
-            -> PublicKey
-            -- ^ The public key of the sender of the encrypted thing
-            -> PrivateKey
-            -- ^ The private key of the recipient of the encrypted thing
-            -> ByteData;
-decryptWKey crip pu pr = error "decryptWKey is unimplemented.";
 
 -- | @calcSecret a b@ is the shared secret key of @a@ and @b@.
 calcSecret :: PublicKey
--- src/Metal/MatrixAPI/LowLevel/Crypto/Olm.hs
+++ src/Metal/MatrixAPI/LowLevel/Crypto/Olm.hs
@@ -9,4 +9,44 @@
 -- Metal.LowLevel.MatrixAPI.Crypto.Olm contains some functions which
 -- implement the Olm cryptographic protocol such that messages and
 -- whatnot can be properly encrypted.
-module Metal.LowLevel.MatrixAPI.Crypto.Olm where
+module Metal.LowLevel.MatrixAPI.Crypto.Olm (
+  encryptWKey,
+  decryptWKey
+) where
+import Data.Maybe;
+import Metal.Base;
+import Crypto.Error;
+import qualified Data.Text as T;
+import Metal.OftenUsedFunctions;
+import qualified Data.ByteString as BS;
+import qualified Crypto.PubKey.Curve25519 as X25519;
+
+-- | @encryptWKey z pu pr@ encrypts @z@ with the shared secret of public
+-- key @pu@ and private key @pr@, outputting the resulting ciphertext.
+--
+-- @encryptWKey@ is IO-monadic because @encryptWKey@ uses functions
+-- which return pseudorandom stuff, e.g., the AES-256 keys which encrypt
+-- the actual data.
+--
+-- @encryptWKey@ is currently nonfunctional.
+encryptWKey :: ByteData
+            -- ^ The plaintext which should be encrypted
+            -> PublicKey
+            -- ^ The public key of the recipient of the encrypted thing
+            -> PrivateKey
+            -- ^ The private key of the sender of the encrypted thing
+            -> IO CipherByteData;
+encryptWKey text pu pr = error "encryptWKey is unimplemented.";
+
+-- | @decryptWKey z pu pr@ decrypts @z@ with the shared secret of public
+-- key @pu@ and private key @pr@, outputting the resulting cleartext.
+--
+-- @decryptWKey@ is currently nonfunctional.
+decryptWKey :: CipherByteData
+            -- ^ The ciphertext which should be decrypted
+            -> PublicKey
+            -- ^ The public key of the sender of the encrypted thing
+            -> PrivateKey
+            -- ^ The private key of the recipient of the encrypted thing
+            -> ByteData;
+decryptWKey crip pu pr = error "decryptWKey is unimplemented.";
