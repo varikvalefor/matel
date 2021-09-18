b5e3ee06100d36a8d579fa0c9f174a39cb711515
Varik Valefor <varikvalefor@aol.com>
2021-09-18 19:10:20 -0400

    app/matelcli.hs: A central thing replaces a boilerplate function.

--- app/matelcli.hs
+++ app/matelcli.hs
@@ -125,7 +125,7 @@ send k a
     "text"     -> T.getContents >>= \input ->
                   return Def.stdMess {body = input}
     "file"     -> T.getContents >>= \c -> upload c (k !! 1) a >>=
-                  return . either (error . T.unpack) id >>= \uploadID ->
+                  return . processError >>= \uploadID ->
                   return Def.stdMess {
                     msgType = Attach,
                     body = T.pack $ k !! 1,
