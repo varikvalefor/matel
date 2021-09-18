3bb7b1c76115e97669cfb5a91acbbe09f5522c77
Varik Valefor <varikvalefor@aol.com>
2021-09-18 18:19:56 -0400

    src/Metal/MatrixAPI/LowLevel/Crypto.hs: Some redundant imports are chucked.

--- src/Metal/MatrixAPI/LowLevel/Crypto.hs
+++ src/Metal/MatrixAPI/LowLevel/Crypto.hs
@@ -9,15 +9,9 @@
 -- Metal.MatrixAPI.LowLevel.Crypto contains high-level cryptographic
 -- crap.
 module Metal.MatrixAPI.LowLevel.Crypto where
-import Data.Maybe;
 import Metal.Base;
-import Crypto.Error;
 import Metal.Encrypted;
 import Metal.Messages.Standard;
-import qualified Data.Text as T;
-import Metal.OftenUsedFunctions;
-import qualified Data.ByteString as BS;
-import qualified Crypto.PubKey.Curve25519 as X25519;
 
 -- | For all CryptoThing @a@, for all @a@ @b@, there exists a function
 -- @decrypt@ such that @decrypt b@ is a value @a@ which represents the
