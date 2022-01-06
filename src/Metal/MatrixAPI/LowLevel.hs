6e0a4d4ea1aa6dd0e4da99f8c8b398552c6afc67
Varik Valefor <varikvalefor@aol.com>
2022-01-05 21:16:05 -0500

    s/processsing/processing/

--- src/Metal/MatrixAPI/LowLevel.hs
+++ src/Metal/MatrixAPI/LowLevel.hs
@@ -574,7 +574,7 @@ createRoom r publcty = responseToEither <.> TP.req TP.POST [] querr bod
   --
   roomIdOf :: BS.ByteString -> Identifier
   roomIdOf = T.unpack . fromMaybe err . (^? A.key "room_id" . A._String)
-  -- \^ @fromJust@ could be used... but when processsing Nothing,
+  -- \^ @fromJust@ could be used... but when processing Nothing,
   -- @fromJust@ would provide a relatively nondescriptive error message,
   -- and VARIK finds that nondescriptive error messages are crap.  As
   -- such, VARIK elects to use @fromMaybe err@ in favour of @fromJust@.
