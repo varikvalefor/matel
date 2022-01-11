94e96741f23a1c26971fdf9d16d8134a7c34193f
Varik Valefor <varikvalefor@aol.com>
2022-01-10 19:09:57 -0500

    Hard wrapping improves.

--- src/Metal/MatrixAPI/LowLevel/GetRoomInformation.hs
+++ src/Metal/MatrixAPI/LowLevel/GetRoomInformation.hs
@@ -33,9 +33,9 @@ import Metal.MatrixAPI.LowLevel.RecordCombination;
 import Metal.MatrixAPI.LowLevel.ResponseToWhatever;
 import qualified Metal.MatrixAPI.LowLevel.HTTP as TP;
 
--- | @getRoomInformation room a@ returns a 'Room'-based representation of
--- the Matrix room whose internal Matrix ID is specified within @room@
--- if the "members" API query works.
+-- | @getRoomInformation room a@ returns a 'Room'-based representation
+-- of the Matrix room whose internal Matrix ID is specified within
+-- @room@ if the "members" API query works.
 --
 -- @getRoomInformation room a@ otherwise returns a description of the
 -- problem which is encountered when the "members" query is sent to the
