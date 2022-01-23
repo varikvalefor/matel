83a4491cc5d70c991854c60176381b15d0a60126
Varik Valefor <varikvalefor@aol.com>
2022-01-22 22:34:33 -0500

    Some obsolete/unsafe functions are junked.

--- src/Metal/OftenUsedFunctions.hs
+++ src/Metal/OftenUsedFunctions.hs
@@ -12,23 +12,11 @@
 -- numerous modules of Metal.
 module Metal.OftenUsedFunctions where
 import Data.Bool;
-import Metal.Base;
 import Text.StringRandom;
 import Network.HTTP.Simple;
 import qualified Data.Text as T;
 import qualified Data.ByteString as BS;
 import qualified Data.ByteString.Lazy as BSL;
-
--- | @detroit k@ throws an @'error'@ such that a description of @k@ is
--- written to the standard error.
---
--- To ensure that a decent amount of debug information is available if
--- stuff breaks, where appropriate, @detroit@ should be used instead of
--- some custom error-generating thing.  However, as implied by "where
--- appropriate", custom error messages may be used to throw errors for
--- non-'Response' data.
-detroit :: Response BS.ByteString -> a;
-detroit = error . detroit';
 
 -- | @detroit' k@ displays the status code and body of @k@.
 detroit' :: Response BS.ByteString -> String;
@@ -36,37 +24,9 @@ detroit' k = "Thus spake the homeserver: " ++
              show (getResponseStatusCode k) ++ "; " ++
              show (getResponseBody k) ++ ".";
 
--- | @justLeft (Left k) == k@.  @justLeft (Right g)@ throws an error.
---
--- @justLeft@ is used because VARIK does not wish to add junk data to
--- @fromLeft@ statements, for VARIK finds that such junk data looks a
--- bit inelegant, unlike this perfectly-wrapped paragraph.
---
--- Compare with @'justRight'@.
-justLeft :: Either a b -> a;
-justLeft (Left a) = a;
-justLeft (Right _) = error "justLeft is applied to a value of type \
-                     \'Right'!";
-
--- | @justRight (Right k) == k@.  @justRight (Left g)@ throws an error.
---
--- @justRight@ is used because VARIK does not wish to add junk data to
--- @fromRight@ statements, for VARIK finds that such junk data looks a
--- bit inelegant, unlike this perfectly-wrapped paragraph.
---
--- Compare with @'justLeft'@.
-justRight :: Either a b -> b;
-justRight (Right b) = b;
-justRight (Left _) = error "justRight is applied to a value of type 'Left'!";
-
 -- | @(a <.> b) k@ is equivalent to @a <$> b k@.
 (<.>) :: Functor f => (b -> c) -> (a -> f b) -> a -> f c;
 (<.>) a b c = a <$> b c;
-
--- | @processError (Left k)@ throws an error whose error message is @k@.
--- | @processError (Right k) == k@.
-processError :: Either Stringth a -> a;
-processError = either (error . T.unpack) id;
 
 -- | 'StringLike' contains the types which can be converted to and from
 -- 'String's.
@@ -98,12 +58,3 @@ instance StringLike T.Text where
 -- pseudorandom sequences should be sufficient.
 favoriteNoise :: IO String;
 favoriteNoise = T.unpack <$> stringRandomIO "[A-Za-z0-9]{24}";
-
--- | If @x !! n@ exists, then @x !? n@ is 'Just' @x !! n@.  Otherwise,
--- @x !? n@ is 'Nothing'.
-(!?) :: [a]
-     -- ^ This thing is the list from which an element may be fetched.
-     -> Int
-     -- ^ This bit is the index of the element which may be fetchd.
-     -> Maybe a;
-(!?) xs n = bool Nothing (Just $ xs !! n) $ length xs > n;
