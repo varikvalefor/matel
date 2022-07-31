4d8846d1d3acea5b9be790293986a90c65de3220
Varik Valefor <varikvalefor@aol.com>
2022-07-31 13:46:36 -0400

    A test suite for Metal.OftenUsedFunction begins.

---
+++ testes/core/oeuf.hs
@@ -0,0 +1,11 @@
+-- | This test suite determines whether or not the functions which are
+-- defined in "Metal.OftenUsedFunctions" actually work.
+module Main where
+import Data.Bool;
+import System.Exit;
+import Metal.OftenUsedFunctions;
+
+main :: IO ExitCode;
+main = bool exitFailure exitSuccess $ and correctness
+  where
+  correctness = [((++ "b") .: (++)) "bo" "o" == "boob"];
